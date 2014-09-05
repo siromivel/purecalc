04a4c190f3b80eb3b19d6643580e9bfea5057aea
Levi Skye Morris <siromivel@gmail.com>
2014-09-05 12:08:05 -0400

    Added support for keyboard input and cleaned up the code. Improved functionality of the del button.

--- calculator.js
+++ calculator.js
@@ -1,20 +1,14 @@
+//Global variables
 var results = document.getElementById("display");
 var inputs = ["","",""];
 var values = [];
 
-function add(a, b) {
-	return a + b;
-}
-function subtract(a, b) {
-	return a - b;
-}
-function multiply(a, b) {
-	return a * b;
-}
-function divide(a, b) {
-	return a / b;
-}
-function equals() {
+// Arithmetic functions
+var add = function(a, b) {return a + b;}
+var subtract = function(a, b) {return a - b;}
+var multiply = function(a, b) {return a * b;}
+var divide = function(a, b) {return a / b;}
+var equals = function() {
 	if (inputs[1] === "+") {
 		var sum = add(parseFloat(inputs[0]), parseFloat(inputs[2]));
 		clear();	
@@ -37,19 +31,36 @@ function equals() {
 	}
 	display();
 }
-function pusher(value) {
+
+// Functions for storing and displaying user input
+var update = function(value) {
 	inputs.push(value);
 	inputs.shift();
 }
-function clear() {
+var clear = function() {
 	inputs = ["","",""];
 	values = [];
 	display();
 }
-function display() {
+var del = function() {
+  if(values.length > 0) {
+    values.pop();
+  }
+  else {
+    for(var i = 2; i >= 0; i--) {
+      var x = inputs[i]
+      if(inputs[i]) { 
+        var y = x.slice(0,-1); 
+        inputs[i] = y;
+      }
+    }
+  } 
+}
+var display = function() {
 	results.innerHTML = inputs.join(" ") + " " + values.join("");
 }
 
+// Event listeners for mouse input
 for (var i = 0; i < 11; i++) {
 	document.getElementById(i).addEventListener("click", function() {
 		values.push(this.innerHTML);
@@ -58,19 +69,48 @@ for (var i = 0; i < 11; i++) {
 }
 for (var i = 11; i < 15; i++) {
 	document.getElementById(i).addEventListener("click", function() {
-		pusher(values.join(""));
-		pusher(this.innerHTML);
+		update(values.join(""));
+		update(this.innerHTML);
 		values = [];
 		display();
 	});
 }
 document.getElementById(15).addEventListener("click", function(){
-	pusher(values.join(""));
+	update(values.join(""));
 	values = [];
 	equals();
 });
 document.getElementById(16).addEventListener("click", function(){ clear(); });
 document.getElementById(17).addEventListener("click", function(){ 
-	values.pop();
+  del();
 	display();
+});
+
+//Keyboard support
+document.addEventListener('keydown', function(event) {
+  if((event.keyCode > 47) && (event.keyCode < 58))  { values.push(event.keyCode - 48);
+  } else if((event.keyCode > 95) && (event.keyCode < 106)) { values.push(event.keyCode - 96); 
+  } else if(event.keyCode === 106) {
+      update(values.join(""));
+      update("*");
+      values = [];
+  } else if(event.keyCode === 107) {
+      update(values.join(""));
+      update("+");
+      values = [];
+  } else if(event.keyCode === 109) {
+      update(values.join(""));
+      update("-");
+      values = [];
+  } else if(event.keyCode === 110) { values.push(".");
+  } else if(event.keyCode === 111) {
+      update(values.join(""));
+      update("/");
+      values = [];
+  } else if(event.keyCode === 12 || 13) {
+    	update(values.join(""));
+      values = [];
+      equals();
+  }
+  display();
 });
